474c6a5d828e6d3b4d6b5d15dfb73f5faae92a60
chaoticgoodpanda <lwinm@seas.upenn.edu>
2022-04-24 15:26:47 +0200

    Set up home screen with bottom navigation menu bar

--- lib/mainScreens/main_screen.dart
+++ lib/mainScreens/main_screen.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:taxi_bookings_app/tabPages/earning_tab.dart';
+import 'package:taxi_bookings_app/tabPages/home_tab.dart';
+import 'package:taxi_bookings_app/tabPages/profile_tab.dart';
+import 'package:taxi_bookings_app/tabPages/ratings_tab.dart';
 
 class MainScreen extends StatefulWidget {
   const MainScreen({Key? key}) : super(key: key);
@@ -7,12 +11,68 @@ class MainScreen extends StatefulWidget {
   State<MainScreen> createState() => _MainScreenState();
 }
 
-class _MainScreenState extends State<MainScreen> {
+class _MainScreenState extends State<MainScreen> with SingleTickerProviderStateMixin {
+
+  TabController? tabController;
+  int selectedIndex = 0;
+
+  onItemClicked(int index)
+  {
+    setState(() {
+      selectedIndex = index;
+      tabController!.index = selectedIndex;
+    });
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    // length = 4 because we are creating 4 pages
+    tabController = TabController(length: 4, vsync: this);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Welcome"),
+      body: TabBarView(
+        physics: NeverScrollableScrollPhysics(),
+        controller: tabController,
+        children: const [
+          HomeTabPage(),
+          EarningsTabPage(),
+          RatingsTabPage(),
+          ProfileTabPage(),
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: "Home",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.credit_card),
+            label: "Earnings",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.star),
+            label: "Ratings",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: "Account",
+          ),
+        ],
+        unselectedItemColor: Colors.white54,
+        selectedItemColor: Colors.white,
+        backgroundColor: Colors.black,
+        type: BottomNavigationBarType.fixed,
+        selectedLabelStyle: TextStyle(fontSize: 14),
+        showUnselectedLabels: true,
+        currentIndex: selectedIndex,
+        onTap: onItemClicked,
       ),
     );
   }
